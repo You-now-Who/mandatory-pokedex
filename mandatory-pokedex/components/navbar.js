c38e8c5c515aab18f36c8c7043373cb68d37cdf0
Tarun <nottarun7@gmail.com>
2024-04-08 16:59:18 +0530

    search page

--- mandatory-pokedex/components/navbar.js
+++ mandatory-pokedex/components/navbar.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-700">
+    <nav className="bg-white border-gray-200 dark:bg-indigo-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <div className="bg-white dark:bg-gray-700 rounded-full p-1">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png" className="h-8" alt="Pokeball logo" />
-          </div>
-          <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-red-700">Pokedex</span>
-        </a>
-        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-700 dark:border-gray-700">
+        <Link href="/">
+          <span className="flex items-center space-x-3 rtl:space-x-reverse">
+            <div className="bg-white dark:bg-indigo-900 rounded-full p-1">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png" className="h-8" alt="Pokeball logo" />
+            </div>
+            <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-red-600">Pokedex</span>
+          </span>
+        </Link>
+        <div className="hidden md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:bg-white dark:bg-gray-800 md:dark:bg-indigo-900 dark:border-indigo-900">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0 dark:text-white md:dark:text-red-500" aria-current="page">Search</a>
+              <Link href="/search">
+                <span className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-indigo-900 dark:hover:text-white md:dark:hover:bg-transparent">Search</span>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Compare</a>
+              <Link href="/compare">
+                <span className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-indigo-900 dark:hover:text-white md:dark:hover:bg-transparent">Compare</span>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pokemon</a>
+              <Link href="/pokemons">
+                <span className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-indigo-900 dark:hover:text-white md:dark:hover:bg-transparent">Pokemon</span>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Favorites</a>
+              <Link href="/favourites">
+                <span className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-indigo-900 dark:hover:text-white md:dark:hover:bg-transparent">Favourites</span>
+              </Link>
             </li>
           </ul>
         </div>
